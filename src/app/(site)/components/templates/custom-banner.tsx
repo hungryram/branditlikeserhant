ba5a288ea83309839250ec38cf9fce4b37f9477e
Ram Dettmer <ram@hungryram.com>
2023-09-19 13:55:12 -0700

    update

--- src/app/(site)/components/templates/custom-banner.tsx
+++ src/app/(site)/components/templates/custom-banner.tsx
@@ -1,6 +1,6 @@
 export default function CustomBanner() {
     return (
-        <div className="py-6">
+        <div className="py-6 p">
             <div className="md:px-12 px-4 text-center content">
                 <p className="text-xl">2-Time International Bestselling Author Ryan Serhant proudly presents his <span className="font-bold">third</span> book: <a href="#buy" target="_blank"><span className="text-[#264495] font-bold uppercase">Brand it Like Serhant</span></a></p>
             </div>
